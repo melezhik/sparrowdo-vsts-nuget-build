6b547ce3efd893f51c5b4531ec49ac4f2969b5fd
Alexey Melezhik <melezhik@gmail.com>
2018-07-16 22:11:07 +0000

    remove old file

--- resources/nuget-pack.bat
+++
@@ -1,7 +0,0 @@
-set nuget_bin=%1
-set configuration=%2
-set project_file=%3
-set output_directory=%4
-
-%nuget_bin% pack %project_file% -Properties Configuration=%configuration% -OutputDirectory %output_directory% -Build
-
